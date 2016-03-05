0395b23794bb1264f29ea7b5d409e8d581fafc7b
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2016-03-05 01:12:57 +0100

    Angular project created

---
+++ script.js
@@ -0,0 +1,14 @@
+var app = angular.module("computer", ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/main', {
+            templateUrl: 'main.html',
+            controller: 'MainCtrl'
+        }).otherwise({
+            redirectTo: '/main'
+        })
+    }])
+
+    .controller('MainCtrl', ['$scope', function ($scope) {
+        $scope.person = 'John Doe';
+        console.log($scope);
+    }]);
